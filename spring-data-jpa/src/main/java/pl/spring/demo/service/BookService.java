2575d4cc181835131ca8febe3d720609d812f62c
michalchrapczynski <michal.chrapczynski@gmail.com>
2015-11-10 10:39:10 +0100

    dodano tabele ksiazek, dodano przycisk delete oraz utworzono jego fukcjonalnosc
    w funkcjonalnosc przycisku delete jest załączona informacja zwrotna o
    usuniętej książce

--- spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
+++ spring-data-jpa/src/main/java/pl/spring/demo/service/BookService.java
@@ -1,14 +1,18 @@
 package pl.spring.demo.service;
 
-import pl.spring.demo.to.BookTo;
-
 import java.util.List;
+
+import pl.spring.demo.to.BookTo;
 
 public interface BookService {
 
-    List<BookTo> findAllBooks();
-    List<BookTo> findBooksByTitle(String title);
-    List<BookTo> findBooksByAuthor(String author);
+	List<BookTo> findAllBooks();
 
-    BookTo saveBook(BookTo book);
+	List<BookTo> findBooksByTitle(String title);
+
+	List<BookTo> findBooksByAuthor(String author);
+
+	BookTo saveBook(BookTo book);
+
+	void removeBook(BookTo book);
 }
--- web/src/main/java/pl/spring/demo/web/controller/BookController.java
+++ web/src/main/java/pl/spring/demo/web/controller/BookController.java
@@ -1,24 +1,48 @@
 package pl.spring.demo.web.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import pl.spring.demo.service.BookService;
-import pl.spring.demo.to.BookTo;
-
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import pl.spring.demo.service.BookService;
+import pl.spring.demo.to.BookTo;
+
 @Controller
 public class BookController {
-    @Autowired
-    private BookService bookService;
+	@Autowired
+	private BookService bookService;
 
-    @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public String bookList(Map<String, Object> params) {
-        final List<BookTo> allBooks = bookService.findAllBooks();
-        params.put("books", allBooks);
-        return "bookList";
-    }
+	private List<BookTo> allBooks;
+
+	@RequestMapping(value = "/books", method = RequestMethod.GET)
+	public String bookList(Map<String, Object> params) {
+		// ModelAndView mav = new ModelAndView("bookList");
+		// mav.addObject("books", allBooks);
+		allBooks = bookService.findAllBooks();
+		params.put("books", allBooks);
+		return "bookList";
+	}
+
+	@RequestMapping(value = "/bookDelete/{id}", method = RequestMethod.GET)
+	public String bookDelate(@ModelAttribute(value = "id") Long id, Map<String, Object> params) {
+		if (!allBooks.isEmpty()) {
+			for (BookTo bookTo : allBooks) {
+				if (bookTo.getId() == id) {
+					bookService.removeBook(bookTo);
+					params.put("bookDelete", bookTo);
+					break;
+				}
+			}
+		}
+		allBooks = bookService.findAllBooks();
+		params.put("books", allBooks);
+
+		return "bookDelete";
+
+	}
 }
